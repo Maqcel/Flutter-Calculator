ff5c4bd6bf14fa40a33faf324a4c6669cfafae53
Maqcel <marcel.kozien@gmail.com>
2020-05-12 20:42:42 +0200

    Rebuild Buttons & Solving
    Left to do for actual version change last number sign

--- lib/content.dart
+++ lib/content.dart
@@ -1,46 +1,50 @@
 import 'package:flutter/material.dart';
 
-Widget buildContainer(Size size, List<Object> content, int index,Function tapped) {
-    return Container(
-      child: InkWell(
-        child: Center(
-          child: Container(
-            decoration: BoxDecoration(
-                color: Colors.grey,
-                borderRadius: BorderRadius.all(Radius.circular(5))),
-            child: InkWell(
-              child: Center(
-                child: content[index] is Icon
-                    ? content[index]
-                    : Text(
-                        content[index].toString(),
-                        style: TextStyle(fontSize: 30),
-                      ),
-              ),
-              borderRadius: BorderRadius.circular(15),
-            ),
-            margin: EdgeInsets.only(top: 5),
-            width: size.width * 0.24,
-            height: size.width * 0.24,
-          ),
-        ),
-        borderRadius: BorderRadius.circular(15),
-        onTap:() => tapped(content,index),
+Widget buildContainer(
+    Size size, List<Object> content, int index, Function tapped) {
+  return Container(
+    margin: EdgeInsets.all(size.height * 0.0024),
+    width: size.width * 0.24,
+    height: size.width * 0.24,
+    decoration: BoxDecoration(
+      color: Colors.grey,
+      borderRadius: BorderRadius.all(
+        Radius.circular(5),
       ),
-      width: size.width * 0.25,
-      height: size.width * 0.25,
-    );
-  }
+    ),
+    child: FlatButton(
+      child: Center(
+        child: content[index] is Icon
+            ? content[index]
+            : Text(
+                content[index].toString(),
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+      ),
+      onPressed: () => tapped(content, index),
+    ),
+  );
+}
 
-  Widget buildColumn(Size size, List<Object> content, int index,Function tapped) {
-    return Column(
-      children: <Widget>[
-        buildContainer(size, content, index,tapped),
-        buildContainer(size, content, index + 1,tapped),
-        buildContainer(size, content, index + 2,tapped),
-        buildContainer(size, content, index + 3,tapped),
-        buildContainer(size, content, index + 4,tapped),
-      ],
-      crossAxisAlignment: CrossAxisAlignment.center,
-    );
-  }
+Widget buildColumn(
+    Size size, List<Object> content, int index, Function tapped) {
+  return Column(
+    children: <Widget>[
+      buildContainer(size, content, index, tapped),
+      buildContainer(size, content, index + 1, tapped),
+      buildContainer(size, content, index + 2, tapped),
+      buildContainer(size, content, index + 3, tapped),
+      buildContainer(size, content, index + 4, tapped),
+    ],
+    crossAxisAlignment: CrossAxisAlignment.center,
+  );
+}
+bool isDigit(String s, int idx) => (s.codeUnitAt(idx) ^ 0x30) <= 9;
+String changeSign(String exp){
+  String modified = exp;
+  bool cond = true;
+   
+  return  modified;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'content.dart';
 
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 void main() {
   runApp(MyApp());
@@ -36,14 +37,53 @@ class _MyHomeScreenState extends State<MyHomeScreen> {
     //3
     Icon(MdiIcons.percent), 9, 6, 3, '.',
     //4
-    Icon(MdiIcons.division), Icon(MdiIcons.closeThick), Icon(MdiIcons.minus),
-    Icon(MdiIcons.plusThick), Icon(MdiIcons.equal)
+    Icon(MdiIcons.division), Icon(MdiIcons.closeThick), Icon(MdiIcons.minus), Icon(MdiIcons.plusThick), Icon(MdiIcons.equal),
   ];
 
-  bool hover = false;
+  String equation = '';
+  String result = '0';
+  String expression = '';
 
-  void _tapped(List<Object> content,int index){
-    print(content[index]);
+  void _tapped(List<Object> content, int index) {
+    setState(() {
+      if (content[index] == content[0] ) { //clear
+        equation = '';
+        result = '0';
+      } else if (content[index] == content[5]) { //backspace
+        equation = equation.substring(0, equation.length - 1);
+        if (equation.isEmpty) equation = '0';
+      } else if (content[index] == content[19]) { // =
+        expression = equation;
+        equation = '';
+        expression = expression.replaceAll('×', '*');
+        expression = expression.replaceAll('÷', '/');
+        try{
+          Parser p = new Parser();
+          Expression exp = p.parse(expression);
+
+          ContextModel cm = ContextModel();
+          result = '${exp.evaluate(EvaluationType.REAL, cm)}';
+        }catch(e){
+          result = 'ERROR';
+        }
+
+      } else {
+        if (content[index] == content[10]) {
+          equation += '%';
+        } else if (content[index] == content[15]) {
+          equation += '÷';
+        } else if (content[index] == content[16]) {
+          equation += '×';
+        } else if (content[index] == content[17]) {
+          equation += '-';
+        } else if (content[index] == content[18]) {
+          equation += '+';
+        } else {
+          equation += content[index].toString();
+        }
+      }
+      //print(equation);
+    });
   }
 
   @override
@@ -58,6 +98,27 @@ class _MyHomeScreenState extends State<MyHomeScreen> {
         child: Column(
           children: <Widget>[
             Container(
+              child: ListView(
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      equation.isEmpty ? '0' : equation,
+                      style: TextStyle(fontSize: 40),
+                    ),
+                  ),
+                  Divider(),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      result,
+                      style: TextStyle(fontSize: 40, color: Colors.black54),
+                    ),
+                  ),
+                ],
+                //crossAxisAlignment: CrossAxisAlignment.end,
+              ),
+              padding: EdgeInsets.symmetric(horizontal: 10),
               color: Colors.black54,
               height: _screenSize.height * 0.28,
             ),
@@ -66,10 +127,10 @@ class _MyHomeScreenState extends State<MyHomeScreen> {
                 color: Colors.black87,
                 child: Row(
                   children: <Widget>[
-                    buildColumn(_screenSize, content, 0,_tapped),
-                    buildColumn(_screenSize, content, 5,_tapped),
-                    buildColumn(_screenSize, content, 10,_tapped),
-                    buildColumn(_screenSize, content, 15,_tapped),
+                    buildColumn(_screenSize, content, 0, _tapped),
+                    buildColumn(_screenSize, content, 5, _tapped),
+                    buildColumn(_screenSize, content, 10, _tapped),
+                    buildColumn(_screenSize, content, 15, _tapped),
                   ],
                 ),
               ),
